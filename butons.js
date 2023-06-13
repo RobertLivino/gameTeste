cd8e411eaaa8554f02886bd9c7369fb34c007ef1
Robert Livino <robertlivino08@gmail.com>
2023-06-13 13:34:39 -0300

    commit prevenção

--- butons.js
+++ butons.js
@@ -1,13 +1,16 @@
 (function () {
+    const me = this;
+    const topbarItems = document.getElementById("topbarItems")
     const mousePosText = document.getElementById('mouse-pos');
     let mousePos = { x: undefined, y: undefined };
 
     window.addEventListener('mousemove', (event) => {
         mousePos = { x: event.clientX, y: event.clientY };
         mousePosText.textContent = `(${mousePos.x}, ${mousePos.y})`;
+        if (mousePos) {
+            /* //<debug>
+            debugger;
+            //</debug> */
+        }
     });
 })();
-
-window.addEventListener('mousemove', (event) => {
-    mousePos = { x: event.clientX, y: event.clientY };
-});
