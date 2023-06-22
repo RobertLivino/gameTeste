fba8c5f0571bd0816100b1236167377d54622631
Robert Livino <robertlivino08@gmail.com>
2023-06-22 18:02:11 -0300

    ball movement finish

--- BallScript.js
+++ BallScript.js
@@ -1,11 +1,19 @@
+let speed;
+let maxSpeed = { x: 0, y: 0 };
+let isDragging = false;
+let bollFalling = false;
+let alreadyPointed = false;
 const basket = document.getElementById('basket');
 const basketPos = document.getElementById('basket-pos');
 
 const basketBall = document.getElementById('basket-Ball');
 const ballPos = document.getElementById('ball-pos');
+
+const playerScore = document.getElementById('player-score');
 (function () {
     const mousePosText = document.getElementById('mouse-pos');
-
+    const mouseVelText = document.getElementById('mouse-vel');
+    const mouseMaxVel = document.getElementById('mouse-max-vel');
     let mousePos = { x: undefined, y: undefined };
 
     basketPos.textContent = `(${basket.offsetLeft}, ${basket.offsetTop})`;
@@ -13,17 +21,49 @@ const ballPos = document.getElementById('ball-pos');
     window.addEventListener('mousemove', (event) => {
         mousePos = { x: event.clientX, y: event.clientY };
         mousePosText.textContent = `(${mousePos.x}, ${mousePos.y})`;
+        speed = { x: event.movementX, y: event.movementY };
+
+        if (speed.x > maxSpeed.x) {
+            maxSpeed.x = speed.x;
+        };
+        if (speed.y > maxSpeed.y) {
+            maxSpeed.y = speed.y;
+        }
+        if (speed.x < 0) {
+            if (speed.x < maxSpeed.x) {
+                maxSpeed.x = speed.x;
+            }
+        };
+        if (speed.y < 0) {
+            if (speed.y < maxSpeed.y) {
+                maxSpeed.y = speed.y;
+            }
+        };
+        mouseVelText.textContent = `(${speed.x}, ${speed.y})`;
+        mouseMaxVel.textContent = `(${maxSpeed.x}, ${maxSpeed.y})`;
     });
+    basketBall.addEventListener('mouseup', toggleBollMoveTrue);
+    basketBall.addEventListener('mousedown', toggleBollMoveFalse);
 })();
 
-dragElement(basketBall);
-basketBall.addEventListener('mouseup', bollFallingDown);
+
+function toggleBollMoveTrue() {
+    bollFalling = true;
+    isDragging = false;
+}
+function toggleBollMoveFalse() {
+    dragElement(basketBall);
+    bollFalling = false;
+}
 
 function dragElement(element) {
     let pos1 = 0;
     let pos2 = 0;
     let pos3 = 0;
     let pos4 = 0;
+    if (!element) {
+        return;
+    }
     element.onmousedown = dragMouseDown;
 
     function dragMouseDown(e) {
@@ -64,18 +104,23 @@ function dragElement(element) {
     function closeDragElement() {
         document.onmouseup = null;
         document.onmousemove = null;
-        bollFallingDown(pos3, pos4);
+        bollFallingDown(basketBall.offsetLeft, basketBall.offsetTop);
     }
 }
 function bollFallingDown(x, y) {
     const gravity = 0.1;
     let position = { x: x, y: y };
-    let velocity = { x: 0, y: 0 };
+    let velocity = { x: maxSpeed.x, y: maxSpeed.y };
     const speedScale = 0.5;
+    bollFalling = true;
     update(basketBall, gravity, position, velocity, speedScale);
 }
 function update(object, gravity, position, velocity, speedScale) {
     ballPos.textContent = `(${basketBall.offsetLeft}, ${basketBall.offsetTop})`;
+    if (!bollFalling) {
+        dragElement(object);
+        return;
+    }
     if (!object) {
         return;
     }
@@ -88,27 +133,49 @@ function update(object, gravity, position, velocity, speedScale) {
     if (!velocity) {
         return;
     }
-
-    // Atualize a posição do objeto com base na velocidade
+    let basketHeightReference = position.y >= basket.offsetTop && position.y <= (basket.offsetTop + basket.offsetHeight) ? true : false;
+    let basketWidthReference = position.x >= basket.offsetLeft && position.x <= (basket.offsetLeft + basket.offsetWidth) ? true : false;
+    if (basketHeightReference && basketWidthReference) {
+        if (!alreadyPointed) {
+            alreadyPointed = true;
+            playerScore.textContent++;
+        }
+    }
+    if (!basketHeightReference) {
+        if (alreadyPointed) {
+            alreadyPointed = false;
+        }
+    }
     position.x += velocity.x * speedScale;
     position.y += velocity.y * speedScale;
 
-    // Aplique a gravidade à velocidade
     velocity.y += gravity;
 
-    // Verifique se o objeto atingiu o chão
-    if ((position.y + object.offsetTop + object.offsetHeight) >= window.innerHeight) {
-        // Inverta a velocidade vertical para simular um efeito de salto
+    if ((position.y + object.offsetHeight) >= window.innerHeight) {
         velocity.y *= -0.8;
 
-        // Mantenha o objeto acima do chão
-        position.y = window.innerHeight - object.offsetHeight - object.offsetTop;
+        position.y = window.innerHeight - object.offsetHeight;
+    }
+    if (position.y <= 0) {
+        velocity.y *= -0.8;
+
+        position.y = 0;
+    }
+    if ((position.x + object.offsetWidth) >= window.innerWidth) {
+        velocity.x *= -0.8;
+
+        position.x = window.innerWidth - object.offsetWidth;
+    }
+    if (position.x <= 0) {
+        velocity.x *= -0.8;
+
+        position.x = 0;
     }
 
-    // Atualize a posição do elemento HTML
-    object.style.transform = `translate(${position.x}px, ${position.y}px)`;
+    object.style.top = position.y + "px";
+    object.style.left = position.x + "px";
 
-    // Agende a próxima atualização
+    bollFalling = true;
     requestAnimationFrame(() => update(object, gravity, position, velocity, speedScale));
 }
 update();
